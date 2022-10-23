c56144ee2695266082a291cc073fd21e017e0b8e
alexisarte <alexisrodriguezarteaga@gmail.com>
2022-10-23 20:12:24 -0300

    Settings page

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,19 @@
 import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import './App.css'
+import Settings from './Settings';
+import Timer from './Timer';
+
 
 function App() {
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
 
-  function decreaseSeconds() {
-    setSeconds(seconds => seconds + 1);
-  }
-
-  function resetSeconds() {
-    setSeconds(seconds => seconds - 59);
-  }
+  const [showSettings, setShowSettings] = useState(true);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            let action;
-            if (seconds < 59) {
-              action = decreaseSeconds;
-            } else if (seconds === 59) {
-              action = resetSeconds;
-            }
-            console.log(seconds)
-            setInterval(action, 1000);
-          }}
-        >
-          seconds is {seconds}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <main>
+      {showSettings ? <Settings /> : <Timer />}
+      
+
+    </main>
   );
 }
 
